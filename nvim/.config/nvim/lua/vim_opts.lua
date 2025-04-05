f8954c631552e02b566170ba5f93ed17ced0d089
Florian Fischer <florian.fischer-2@mni.thm.de>
2025-04-05 15:32:28 +0200

    adds missing settings for vim

--- nvim/.config/nvim/lua/vim_opts.lua
+++ nvim/.config/nvim/lua/vim_opts.lua
@@ -4,3 +4,22 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.keymap.set('i', 'jj', '<Esc>')
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 150
+vim.opt.timeoutlen = 300
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
