c5f17c474f7f1ee69e11af46a7b40ba52d65d2ab
Florian Fischer <florian.fischer-2@mni.thm.de>
2025-04-05 13:47:40 +0200

    first iteration to setup neovim with lsp and completion

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "ruff"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({capabilities = capabilities})
+      lspconfig.ruff.setup({capabilities = capabilities})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,{})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation,{})
+      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition,{})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename,{})
+      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action,{})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references,{})
+    end
+  }
+}
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- fill any relevant options here
+    },
+  config = function()
+    vim.keymap.set('n', '<C-p>', ":Neotree filesystem reveal left<CR>", { desc = 'Shows the file tree on the left of the screen' })
+  end
+}
