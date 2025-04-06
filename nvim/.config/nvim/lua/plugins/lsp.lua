ba5b7483203f1a5d1afdb2aa6545c62fa76581b4
Florian Fischer <florian.fischer-2@mni.thm.de>
2025-04-06 12:57:38 +0200

    adds new lsp and some new keymaps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "ruff", "gopls"}
+        ensure_installed = {"lua_ls", "ruff", "gopls", "pyright"}
       })
     end
   },
@@ -21,14 +21,16 @@ return {
       lspconfig.lua_ls.setup({capabilities = capabilities})
       lspconfig.ruff.setup({capabilities = capabilities})
       lspconfig.gopls.setup({capabilities = capabilities})
+      lspconfig.pyright.setup({capabilities = capabilities})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation,{})
-      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition,{})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename,{})
-      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action,{})
+      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition,{})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,{})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action,{})
       vim.keymap.set('n', 'gr', vim.lsp.buf.references,{})
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
     end
   }
 }
